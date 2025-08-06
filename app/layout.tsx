71fe049496f386b2da27e3e0fbabb4fec0766a46
jenish <jenish25791@gmail.com>
2025-08-06 18:11:31 +0530

    Metadata Changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,8 @@
-
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { MyWrapper } from "@/lib/edge-config-context";
 import { getSiteConfig } from "@/lib/edge-config";
-import type { Metadata } from "next";
+import type { Metadata, ResolvingMetadata } from "next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -14,6 +13,7 @@ const geistMono = Geist_Mono({
   variable: "--font-geist-mono",
   subsets: ["latin"],
 });
+
 
 
 
@@ -29,16 +29,19 @@ export const metadata: Metadata = {
     "Dr Bela Donga",
     "Dr Radhika Patoriya"
   ],
-  // icons:{
-  //   icon: [
-  //     { url: "/ic/favicon-32x32.png", sizes: "32x32", type: "image/png" },
-  //     { url: "/ic/favicon-16x16.png", sizes: "16x16", type: "image/png" },
-  //   ],
-  //   apple: [
-  //     { url: "/ic/apple-icon-180x180.png", sizes: "180x180" },
-  //     { url: "/ic/apple-icon-152x152.png", sizes: "152x152" },
-  //   ],
-  // },
+  openGraph: {
+    title: "Veda Multispeciality Clinic - Expert Medical Care in South Bopal, Ahmedabad",
+    description: "Veda Multi Speciality Clinic offers expert medical care with trusted specialists including ENT surgeons , orthopaedics, cardiologists, and more. Led by Dr. Bela Donga and Dr. Radhika Patoriya.",
+    url: "https://veda-clinic-website.vercel.app",
+    images: [
+      {
+        "url": "https://veda-clinic-website.vercel.app/b4dac46e-f71a-442e-be96-f7a841eaaf16.png",
+        "height": 416,
+        "width": 1022,
+        "alt": "Veda Multispeciality Clinic South Bopal Ahmedabad"
+      }
+    ]
+  }
 };
 
 export default async function RootLayout({
--- app/page.tsx
+++ app/page.tsx
@@ -5,36 +5,36 @@ import Footer from "@/components/Front/Footer";
 import Header from "@/components/Front/Header";
 import Hero from "@/components/Front/Hero";
 import Services from "@/components/Front/Services";
-import { getAllArrayData, getSiteConfig } from "@/lib/edge-config";
-import type { Metadata, ResolvingMetadata } from "next";
+import { getAllArrayData } from "@/lib/edge-config";
+// import type { Metadata, ResolvingMetadata } from "next";
 
 
 
-type Props = {
-  params: Promise<{ id: string }>
-  searchParams: Promise<{ [key: string]: string | string[] | undefined }>
-}
+// type Props = {
+//   params: Promise<{ id: string }>
+//   searchParams: Promise<{ [key: string]: string | string[] | undefined }>
+// }
 
 
-export async function generateMetadata(
-  { params, searchParams }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
+// export async function generateMetadata(
+//   { params, searchParams }: Props,
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
 
-  const siteConfig = await getSiteConfig();
+//   const siteConfig = await getSiteConfig();
   
-  return {
-    title: siteConfig.seo.title,
-    description: siteConfig.seo.description,
-    keywords: siteConfig.seo.keywords,
-    openGraph: {
-      title: siteConfig.seo.title,
-      description: siteConfig.seo.description,
-      url: siteConfig.seo.url,
-      images: [...siteConfig.seo.image]
-    },
-  };
-}
+//   return {
+//     title: siteConfig.seo.title,
+//     description: siteConfig.seo.description,
+//     keywords: siteConfig.seo.keywords,
+//     openGraph: {
+//       title: siteConfig.seo.title,
+//       description: siteConfig.seo.description,
+//       url: siteConfig.seo.url,
+//       images: [...siteConfig.seo.image]
+//     },
+//   };
+// }
 
 export default async function Home() {
   const allArrayData = await getAllArrayData();
